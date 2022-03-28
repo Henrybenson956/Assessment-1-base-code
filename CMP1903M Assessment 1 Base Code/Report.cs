c6d46c395a646090374fbdaa3aa6f07efbc7aacf
Henrybenson956 <102376073+Henrybenson956@users.noreply.github.com>
2022-03-28 15:56:20 +0100

    Add files via upload

--- CMP1903M Assessment 1 Base Code/Report.cs
+++ CMP1903M Assessment 1 Base Code/Report.cs
@@ -8,6 +8,22 @@ namespace CMP1903M_Assessment_1_Base_Code
 {
     class Report
     {
+
+
+        String[] sentences = Text.Split('.');
+        int numsentences = sentenses.Count();
+
+        String[] characters = Text.Split('');
+        string[] vowels = ["A","E","I","O","U","a","e","i","o","u"];
+        int numberOfVowls = 0;
+        int numberOfConsonants = 0;
+        int numberOfUpperCase = 0;
+        int numberOfLowerCase = 0;
+        foreach(String character in characters){
+            if(vowels.cont)
+        }
+
+
         //Handles the reporting of the analysis
         //Maybe have different methods for different formats of output?
         //eg.   public void outputConsole(List<int>)
